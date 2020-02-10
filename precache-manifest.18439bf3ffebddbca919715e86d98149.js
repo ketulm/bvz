8f20629353b6138447aa3d58b66905b40d39813b
Ketul Mavani <kkmavani@gmail.com>
2020-02-09 19:51:52 -0500

    boston vision zero visualization application

---
+++ precache-manifest.18439bf3ffebddbca919715e86d98149.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e82a6a14b5c69b6ae2d",
+    "url": "/css/app.3d17cbe4.css"
+  },
+  {
+    "revision": "bc57e69d32df52f9b17e84789e8122a1",
+    "url": "/data/Boston_Neighborhoods.geojson"
+  },
+  {
+    "revision": "948c940e067b924815b2c271715a0794",
+    "url": "/data/Boston_Police_Stations.geojson"
+  },
+  {
+    "revision": "373e5c018ddb525060e9eebd2b956a37",
+    "url": "/data/City_of_Boston_Boundary.geojson"
+  },
+  {
+    "revision": "eb65d50a0194e42cd544b3c6f6af22e3",
+    "url": "/data/Police_Districts.geojson"
+  },
+  {
+    "revision": "fa2ad4fc4c41bafa651a2a781d98ee4b",
+    "url": "/data/crime_data.json"
+  },
+  {
+    "revision": "cf9db68f2639d60815ca9a774a7a0916",
+    "url": "/data/vision_zero_crashes.json"
+  },
+  {
+    "revision": "9fdf9981832b16e8222063c354be0508",
+    "url": "/data/vision_zero_ss.json"
+  },
+  {
+    "revision": "19cb4fa0b7250df6870a4dab92e11c2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e82a6a14b5c69b6ae2d",
+    "url": "/js/app.10dfc934.js"
+  },
+  {
+    "revision": "3e35e1471d4a19491ab0",
+    "url": "/js/chunk-vendors.a490c620.js"
+  },
+  {
+    "revision": "5279259f1808116e405e62be4c4af23d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
